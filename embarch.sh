2b97e03cf38ebb822a65371c1e04f4c871494de0
martoko <18300234+martoko@users.noreply.github.com>
2022-04-09 09:21:28 +0200

    Add mupdf

--- embarch.sh
+++ embarch.sh
@@ -21,7 +21,9 @@ echo 'Changing the default /bin/sh -> /bin/bash symlink to use dash instead'
 sudo ln -sfT dash /usr/bin/sh
 
 
-install_source dwm
-install_source st
-install_source surf
-install_source mpv
+install_source dwm # Window manager
+install_source st # Terminal emulator
+install_source surf # Browser
+install_source mpv # Video/audio player
+
+install_repo mupdf # PDF/comic viewer
